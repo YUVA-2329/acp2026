4a4b02666379f0dedfdaafc25400b52b48125f84
YUVA-2329 <217462938+YUVA-2329@users.noreply.github.com>
2026-03-19 17:26:23 +0530

    demo1 - First attempt and small synatx erors

---
+++ level1/demo1.c
@@ -0,0 +1,49 @@
+/*Write a modular C program to read n float values from the user, store them in an array and find the maximum value and its index. (array, passing arrays to functions, for loop, if statement)
+Function prototype:
+void input(int n, float arr[n]);
+int find_max_index(int n, float arr[n]);
+void output(float arr[ ], int max_index);*/
+//functions prototype
+#include <stdio.h>
+void input(int n, float arr[n]);
+int find_max_index(int n, float arr[]);
+void output(float arr[],int max_index);
+int main(){
+    int n=0;
+    printf("ENTER THE  NUMBER OF ELEMENTS IN THE ARRAY\n");
+    scanf("%d",&n);
+   float arr[n];
+   input(n,arr);
+   int max_index;
+   max_index=find_max_index(n, arr);
+   output(arr,max_index);
+
+
+}
+void input(int n, float arr[n]){
+    int i=0;
+    for(i=0;i<n;i++){
+    printf(
+        "ENTER THE ELEMNT IN THE ARRAY OF THE INDEX %d",arr[i]
+    );
+    scanf("%d",&arr[i]);
+}
+}
+int find_max_index(int n, float arr[n]){
+
+    int i=0;
+    int max=arr[0];
+    int max_index=0;
+  for(i=0;i<n;i++){
+    if(arr[i]>max)
+    max_index=i;
+
+
+}
+return max_index;
+
+
+}
+void output(float arr[],int max_index){
+    printf("THE MAXIMUM VALUE IS %f AND ITS INDEX IS %d",arr[max_index],max_index);
+}
