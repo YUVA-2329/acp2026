b1754f7fcfe056ff7a83905cdad3d82f12ce9e85
YUVA-2329 <217462938+YUVA-2329@users.noreply.github.com>
2026-03-21 21:19:00 +0530

    2ND ATTEMPT WITH CHATGPT

--- level1/demo5.c
+++ level1/demo5.c
@@ -1,42 +1,56 @@
-/*Write a program to swap two integer arrays.   
-Function prototype: 
-void read_array(int n, int arr[]); 
-void print_array(int n, int arr[]); 
-void swap_array(int n, int a[n], int b[n]); */
 #include <stdio.h>
+
 void read_array(int n, int arr[]); 
 void print_array(int n, int arr[]); 
-void swap_array(int n, int a[n], int b[n]);
+void swap_array(int n, int a[], int b[]);
+
 int main(){
-    int n=23;
-    int arr1[n];\
-        int arr2[n];
-    printf("ENTER THE ELEMENTS IN FIRST ARRAY\n");
-    read_array(n,arr1);
-printf("ENTER THE ELEMENTS IN SECOND ARRAY\n");
-    read_array(n,arr2);
-    printf("BEFORE SWAPPING FIRST ARRAY  ");
-    print_array(n,arr1);
-        printf("BEFORE SWAPPING FIRST ARRAY  ");
-    print_array(n,arr2);
-    swap_array(n,arr1,arr2);
+    int n = 5;   // keep small for testing
+    int arr1[n];
+    int arr2[n];
 
-    
+    printf("ENTER ELEMENTS OF FIRST ARRAY:\n");
+    read_array(n, arr1);
+
+    printf("ENTER ELEMENTS OF SECOND ARRAY:\n");
+    read_array(n, arr2);
+
+    printf("BEFORE SWAPPING:\n");
+    printf("Array 1: ");
+    print_array(n, arr1);
+
+    printf("Array 2: ");
+    print_array(n, arr2);
+
+    swap_array(n, arr1, arr2);
+
+    printf("\nAFTER SWAPPING:\n");
+    printf("Array 1: ");
+    print_array(n, arr1);
+
+    printf("Array 2: ");
+    print_array(n, arr2);
+
+    return 0;
 }
+
 void read_array(int n, int arr[]){
-    int i=0;
-    for(i=0;i<n;i++){
-  scanf("%d \n",&arr[i]);}
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);   // FIXED
+    }
 }
-void print_array(int n, int arr[]){
-    int i=0;
-    for(i=0;i<n;i++){
-printf("%s \n",arr[i]);}
-}
-void swap_array(int n, int arr1[n], int arr2[n]){
-    int temp[n]=0;
-    arr1[n]=arr2[n];
-    arr2[n]=temp[n];
-    temp[n]=arr1[n];
 
+void print_array(int n, int arr[]){
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);  // FIXED
+    }
+    printf("\n");
+}
+
+void swap_array(int n, int a[], int b[]){
+    for(int i = 0; i < n; i++){
+        int temp = a[i];
+        a[i] = b[i];
+        b[i] = temp;
+    }
 }
