9869310b0a0610dd751821fe3746e01530cf99dd
YUVA-2329 <yuvakishore.vps@gmail.com>
2026-03-27 22:05:23 +0530

    1 st ATTEMPT

--- level1/bats/act3_test.bats
+++ level1/bats/act3_test.bats
@@ -1,6 +1,6 @@
 #!/usr/bin/env bats
 
 @test "act3 program runs" {
-  run ./act3.exe <<< "1"
+  run ./act3.exe <<< "4"
   [ "$status" -eq 0 ]
 }
---
+++ level1/demo8.c
@@ -0,0 +1,40 @@
+/*8	Write a C program to concatenate two strings using user-defined functions.
+ The program should accept two strings from the user and pass them to a function that performs the concatenation.
+  (strings, while/do while)
+Function prototype:
+void input(char *str);
+void concatenate_strings(char *str1, char *str2);
+void display(char *str);
+*/
+#include <stdio.h>
+#include <string.h>
+void input(char *str);
+void concatenate_strings(char *str1, char *str2);
+void display(char *str);
+int main(){
+   char str1[100] ;
+   char str2[100];
+   
+   printf("ENTER FIRST STRING");
+   input(str1);
+   printf("ENTER SECOND STRING");
+   input(str2);
+   concatenate_strings(str1,str2);
+   display(str1);
+   
+
+return 0;
+
+}
+void input(char *str){
+    fgets(str,100,stdin);
+    str[strcspn(str,"\n")]='\0';
+}
+void concatenate_strings(char *str1, char *str2){
+    strcat(str1,str2);
+    
+}
+void display(char *str){
+  printf("%s",str);
+
+}
