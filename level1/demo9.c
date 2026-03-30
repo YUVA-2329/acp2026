9f0e0950fde20ef91b73d102fde6048f793e5246
YUVA-2329 <yuvakishore.vps@gmail.com>
2026-03-30 12:50:52 +0530

    without gpt

--- level1/demo9.c
+++ level1/demo9.c
@@ -4,8 +4,81 @@ i.	Read the details of n students from the user and store them in an array of st
 ii.	Write the array of structures to a text (ASCII) file using fprintf().
 iii.	Read the data back from the ASCII file into a second array using fscanf() and display it.
 iv.	Display the array of structures.
-Function prototype:
+Function prototype:*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    char name[100];
+    int roll;
+    int marks;
+} Student;
+
 void inputStudents(Student students[], int n);
 void writeToTextFile(Student students[], int n, const char *filename);
 int readFromTextFile(Student students[], const char *filename);
-void printStudents(Student students[], int n);   */
+void printStudents(Student students[], int n);
+
+int main(){
+    int n;
+
+    printf("ENTER THE NUMBER OF STUDENTS: ");
+    scanf("%d",&n);
+
+    Student Stu[n];
+    Student Stu2[n];
+
+    inputStudents(Stu,n);
+
+    writeToTextFile(Stu,n,"students.txt");
+
+    int count = readFromTextFile(Stu2,"students.txt");
+
+    printf("\n--- ORIGINAL DATA ---\n");
+    printStudents(Stu,n);
+
+    printf("\n--- DATA READ FROM FILE ---\n");
+    printStudents(Stu2,count);
+
+    return 0;
+}
+void inputStudents(Student students[], int n) {
+    for(int i = 0; i < n; i++) {
+        printf("\nEnter details of student %d\n", i + 1);
+
+        printf("Name: ");
+        scanf(" %[^\n]", students[i].name); 
+
+        printf("Roll: ");
+        scanf("%d", &students[i].roll);
+
+        printf("Marks: ");
+        scanf("%d", &students[i].marks);
+    }
+}
+void writeToTextFile(Student students[], int n, const char *filename) {
+    FILE *fp = fopen(filename, "w");
+
+    if(fp == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    for(int i = 0; i < n; i++) {
+        fprintf(fp, "%s %d %d\n",
+                students[i].name,
+                students[i].roll,
+                students[i].marks);
+    }
+
+    fclose(fp);
+    printf("\nData written to file successfully.\n");
+}
+void printStudents(Student students[], int n) {
+    for(int i = 0; i < n; i++) {
+        printf("\nStudent %d\n", i + 1);
+        printf("Name: %s\n", students[i].name);
+        printf("Roll: %d\n", students[i].roll);
+        printf("Marks: %d\n", students[i].marks);
+    }
+}
