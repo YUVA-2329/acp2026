db51bd413ad6f633372921ac45bab9dce209e7fb
YUVA-2329 <yuvakishore.vps@gmail.com>
2026-04-02 23:02:51 +0530

    correcting mistakes

--- level1/act8.c
+++ level1/act8.c
@@ -12,30 +12,46 @@ void display(char *str, int vowels, int consonants);
 void input(char *str);
 void count(char *str, int *vowels, int *consonants);
 void display(char *str, int vowels, int consonants);
-int main(){
-char *str;
-int *vowels;
-int *consonants;
-    printf("ENTER THE STRING");
+
+int main() {
+    char str[100];          
+    int vowels = 0;
+    int consonants = 0;
+
+    printf("ENTER THE STRING: ");
     input(str);
-count(str,vowels,consonants);
-display(str,vowels,consonants);
 
+    count(str, &vowels, &consonants);
 
+    display(str, vowels, consonants);
+
+    return 0;
 }
-void input(char *str){
-    scanf("%[^\n]",str);
+
+void input(char *str) {
+    scanf(" %[^\n]", str);   
 }
-void count(char *str,int *vowels,int *consonants){
-  int i=0;
-  while(str[i]!='\0'&&str[i]!='\n'){
-char c=str[i];
-if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
-c=vowels[i];
-else if(c>='a'&&c<='z'||c>='A'&&c<='Z')
-c=consonants[i];
-  }
 
-  }
+void count(char *str, int *vowels, int *consonants) {
+    int i = 0;
 
+    while (str[i] != '\0') {
+        char c = str[i];
 
+        if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+            c=='A'||c=='E'||c=='I'||c=='O'||c=='U') {
+            (*vowels)++;   
+        }
+        else if ((c>='a'&&c<='z') || (c>='A'&&c<='Z')) {
+            (*consonants)++;  
+        }
+
+        i++;   
+    }
+}
+
+void display(char *str, int vowels, int consonants) {
+    printf("\nSTRING: %s\n", str);
+    printf("VOWELS: %d\n", vowels);
+    printf("CONSONANTS: %d\n", consonants);
+}
